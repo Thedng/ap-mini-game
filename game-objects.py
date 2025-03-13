9039ded7001f0cc08ee95e9a145209fe60a43094
Thedng <98289142+Thedng@users.noreply.github.com>
2025-03-13 18:44:48 +0330

    creating basic classes

---
+++ game-objects.py
@@ -0,0 +1,39 @@
+#creat parent class
+class parentClass:
+    def __init__(self,positionx,positiony):
+        # self.position = (round(random.randrange(0, 800),-1), round(random.randrange(0, 600),-1))
+        self.position = (positionx,positiony)
+
+#creat plyer class
+class Player(parentClass):
+    def __init__(self,positionx,positiony):
+        super().__init__(positionx,positiony)
+        self.score = 0
+        self.timer = 0
+    def move(self):
+        pass
+
+    def shoot(self):
+        pass
+
+    def set_score(self):
+        pass
+
+    def set_timer(self):
+        pass
+
+#creat target class
+class target(parentClass):
+    def __init__(self,positionx,positionx):
+        super().__init__(positionx,positionx)
+
+#creat bullet prize class
+class bulletPrize(parentClass):
+    def __init__(self,positionx,positiony):
+        super().__init__(positionx,positiony)
+
+#creat time prize class
+class timePrize(parentClass):
+    def __init__(self,positionx,positiony):
+        super().__init__(positionx,positiony)
+
