985349cce2c9b5a501008f4c3f85b9e52101b53d
danialghr <danial.ghadirian@gmail.com>
2025-03-15 13:33:50 -0700

    score system

--- gameObjects.py
+++ gameObjects.py
@@ -21,7 +21,7 @@ class Player(parentClass):
 
     def set_score(self,p1,p2):
          score = round(math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2),-1)
-        return score
+         self.score =+ score
     def set_timer(self):
         pass
 
