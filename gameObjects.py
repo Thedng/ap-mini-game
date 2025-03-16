4addb868b659765af3b87714c4952a1186467ec7
Thedng <98289142+Thedng@users.noreply.github.com>
2025-03-16 18:04:53 +0330

    fixed some bugs changed some features from main into classes and add bullet limit

--- gameObjects.py
+++ gameObjects.py
@@ -1,20 +1,37 @@
 import math
+import random
 #creat parent class
 class parentClass:
-    def init(self,positionx,positiony,icon):
-        # self.position = (round(random.randrange(0, 800),-1), round(random.randrange(0, 600),-1))
+    def __init__(self,positionx,positiony,icon):
         self.positionx = positionx
         self.positiony = positiony
         self.icon = icon
 
+    def set_position(self):
+        self.positionx = round(random.randrange(50, 750), -1)
+        self.positiony = round(random.randrange(50, 550), -1)
+
+    def move(self,direction,forward):
+        if direction:
+            if forward:
+                self.positionx += 10
+            else:
+                self.positionx -= 10
+        else:
+            if forward:
+                self.positiony += 10
+            else:
+                self.positiony -= 10
+
 #creat plyer class
 class Player(parentClass):
-    def init(self,positionx,positiony,icon):
-        super().init(positionx,positiony,icon)
+    def __init__(self,positionx,positiony,icon):
+        super().__init__(positionx,positiony,icon)
         self.score = 0
         self.timer = 60
         self.shots = [[positionx,positiony]]
         self.bullets = 10
+        self.lastShot = False
 
     def shoot(self):
         self.shots.append([self.positionx,self.positiony])
@@ -22,30 +39,34 @@ class Player(parentClass):
 
     def set_score(self,p1,p2):
         score = round(math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2),-1)
-        if score < 200 :
+        if score < 30 :
             self.score += 1
-        elif score < 400 :
+        elif score < 60 :
             self.score += 2
-        elif score < 600 :
+        elif score < 80 :
             self.score += 3
-        elif score < 800 :
+        elif score < 100 :
             self.sore += 4
         else:
             self.score += 5
+
+    def score_prize(self):
+        self.score += 10
+
     def set_timer(self):
         pass
 
 #creat target class
 class target(parentClass):
-    def init(self,positionx,positiony,icon):
-        super().init(positionx,positiony,icon)
+    def __init__(self,positionx,positiony,icon):
+        super().__init__(positionx,positiony,icon)
 
 #creat bullet prize class
 class bulletPrize(parentClass):
-    def init(self,positionx,positiony,icon):
-        super().init(positionx,positiony,icon)
+    def __init__(self,positionx,positiony,icon):
+        super().__init__(positionx,positiony,icon)
 
 #creat time prize class
 class timePrize(parentClass):
-    def init(self,positionx,positiony,icon):
-        super().init(positionx,positiony,icon)
+    def __init__(self,positionx,positiony,icon):
+        super().__init__(positionx,positiony,icon)
--- main.py
+++ main.py
@@ -30,16 +30,16 @@ def show_shots(obj,position):
 
 #calculate distance
 def distance(obj1,obj2):
-    res = math.sqrt((obj1.positionx-obj2.positionx)  2 + (obj1.positiony-obj2.positiony)  2)
+    res = math.sqrt(((obj1.positionx-2)-(obj2.positionx-11))** 2 + ((obj1.positiony-2)-(obj2.positiony-11))** 2)
     return res
 
 #check if target is hit
 def is_hit(player):
-    if distance(player,target1) < 20:
+    if distance(player,target1) < 10:
         return([True,target1])
-    elif distance(player,target2) < 20:
+    elif distance(player,target2) < 10:
         return([True,target2])
-    elif distance(player,target3) < 20:
+    elif distance(player,target3) < 10:
         return([True,target3])
     return([False,None])
 
@@ -52,36 +52,52 @@ while running:
         #move player 1 and limit
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and player1.positionx > 0:
-                player1.positionx -= 10
+                player1.move(True,False)
             if event.key == pygame.K_RIGHT and player1.positionx < 780:
-                player1.positionx += 10
+                player1.move(True,True)
             if event.key == pygame.K_UP and player1.positiony > 0:
-                player1.positiony -= 10
+                player1.move(False,False)
             if event.key == pygame.K_DOWN and player1.positiony < 580:
-                player1.positiony += 10
-            #player 1 shoot
-            if event.key == pygame.K_SPACE:
+                player1.move(False, True)
+
+            #player 1 shoot target
+            if event.key == pygame.K_SPACE  and player1.bullets > 0 and player1.timer > 0:
                 player1.shoot()
                 res = is_hit(player1)
                 if res[0]:
-                    res[1].positionx = round(random.randrange(50, 800), -1)
-                    res[1].positiony = round(random.randrange(50, 600), -1)
-                    player1.set_score(player1.shots[len(player1.shots) - 2],player1.shots[len(player1.shots) - 1])
-
-        #move player 2 and limit
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_w and player2.positiony > 0:
-                player2.positiony -= 10
-            if event.key == pygame.K_s and player2.positiony < 580:
-                player2.positiony += 10
+                    res[1].set_position()
+                    if player1.lastShot:
+                        player1.score_prize()
+                    else:
+                        player1.set_score(player1.shots[len(player1.shots) - 2],player1.shots[len(player1.shots) - 1])
+                    player1.lastShot = True
+                    print(player1.score)
+                else:
+                    player1.lastShot = False
+            #move player 2 and limit
             if event.key == pygame.K_a and player2.positionx > 0:
-                player2.positionx -= 10
+                player2.move(True, False)
             if event.key == pygame.K_d and player2.positionx < 780:
-                player2.positionx += 10
-            #player 2 shoot
-            if event.key == pygame.K_x:
-                player2.shoot()
+                player2.move(True, True)
+            if event.key == pygame.K_w and player2.positiony > 0:
+                player2.move(False, False)
+            if event.key == pygame.K_s and player2.positiony < 580:
+                player2.move(False, True)
 
+            # player 2 shoot target
+            if event.key == pygame.K_x and player2.bullets > 0 and player2.timer > 0:
+                player2.shoot()
+                res = is_hit(player2)
+                if res[0]:
+                    res[1].set_position()
+                    if player2.lastShot:
+                        player2.score_prize()
+                    else:
+                        player2.set_score(player2.shots[len(player2.shots) - 2], player2.shots[len(player2.shots) - 1])
+                    player2.lastShot = True
+                    print(player2.score)
+                else:
+                    player1.lastShot = False
 
     #set background color
     screen.fill((255, 255, 255))
@@ -90,8 +106,8 @@ while running:
     show_object(target1)
     show_object(target2)
     show_object(target3)
-    for i in player1.shots:
+    for i in player1.shots[1:]:
         show_shots(player1, (i[0],i[1]))
-    for i in player2.shots:
+    for i in player2.shots[1:]:
         show_shots(player2, (i[0],i[1]))
     pygame.display.update()
